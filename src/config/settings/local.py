d4b3d1f1e97c31b45566e1198ce89e4690f143e5
artem-gromov <artem@gromov.kz>
2025-09-13 16:20:21 +0500

    Add S3 storage settings and API schema routes

--- src/config/settings/local.py
+++ src/config/settings/local.py
@@ -1,6 +1,9 @@
-from .base import *  # noqa
+from .base import *  # noqa: F401,F403
+from .base import BASE_DIR
 
 DEBUG = True
 TENANT_DEFAULT_DOMAIN_SUFFIX = ".app.local"
 ALLOWED_HOSTS = ["*"]
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
--- src/config/settings/prod.py
+++ src/config/settings/prod.py
@@ -1,4 +1,10 @@
-from .base import *  # noqa
+from .base import *  # noqa: F401,F403
+from .base import env
 
 DEBUG = False
 
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME", default=None)
